620e90af70ba2fdc31e3f073601504c911939fd0
Professor Lozano <luizmlozano@gmail.com>
2022-09-12 21:16:24 -0300

    codigos finais da aula 1

---
+++ UC8/ER1/exemplo6.js
@@ -0,0 +1,53 @@
+let listaDePecas = ['Filtro de Ar','Roda','Pneu']
+
+listaDePecas.push('Disco de Freio')
+listaDePecas.push('Motor')
+listaDePecas.push('Amortecedor')
+
+//listaDePecas[2] = 'Pastilha' //alterar item da lista pelo indice
+
+if (listaDePecas.length < 10){
+    console.log('É possível cadastrar mais peças!!!')
+}
+else{
+    console.log('Não há mais espaço na caixa')
+}
+
+let peso = 150
+if (peso < 100){
+    console.log('A peça deve pesar no mínimo 100g')
+}
+else{
+    console.log('A peça possui o peso adequado')
+}
+
+let nomePeca = 'Freio'
+
+switch (nomePeca.length) {
+    case 0:
+        console.log('O nome da peça não pode ser vazio')
+        break;
+    case 1:
+    case 2:
+    case 3:
+        console.log('O nome da peça deve ter mais de 3 caracteres')
+        break;
+    default:
+        console.log('O nome da peça esta adequado para o cadastro')
+        break;
+}
+
+
+/*
+if (nomePeca.length > 3){
+    console.log('O nome da peça esta adequado para o cadastro')
+}
+else if(nomePeca.length == 0){
+    console.log("O nome da peça não pode ser vazio.")
+}
+else{
+    console.log("O nome da peça deve ter mais de 3 caracteres, digite o nome adequado")
+}
+*/
+
+
---
+++ UC8/ER1/exemplo7.js
@@ -0,0 +1,11 @@
+var num1;
+let num2;
+
+function somarDoisNumeros(numeroA, numeroB){
+    return numeroA + numeroB
+}
+
+let resultado = somarDoisNumeros(10, 15)
+
+console.log(resultado)
+
