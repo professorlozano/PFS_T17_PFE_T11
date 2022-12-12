c4b8172895cb9fd0c0b6cd59fd799268c6f8f132
Professor Lozano <luizmlozano@gmail.com>
2022-12-12 19:30:41 -0300

    encontro remoto 2

--- UC10/ER2/ER2_concluido/src/app/views/login/login.component.ts
+++ UC10/ER2/ER2_concluido/src/app/views/login/login.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { Router } from '@angular/router';
 import { User } from 'src/app/models/user';
+import { LoginService } from 'src/app/services/login.service';
 
 @Component({
   selector: 'app-login',
@@ -9,16 +10,26 @@ import { User } from 'src/app/models/user';
 })
 export class LoginComponent implements OnInit {
 
+  constructor (private loginService: LoginService, private router: Router){}
+
 ngOnInit(): void {
 }
 
 loginModel = new User();
-
 mensagem = ""
 
 usuarioLogado = ""
 
 onSubmit() {
   console.log(this.loginModel)
+
+  this.loginService.login(this.loginModel).subscribe((response)=> {
+    //console.log("sucesso")
+    this.router.navigateByUrl('')
+  },
+  (respostaErro)=>{
+    this.mensagem = respostaErro.error
+  }
+  )
   }
 }
